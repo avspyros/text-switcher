a3e66016b07c3021473e3e661c6a43950c185121
Spyros <avspyros@gmail.com>
2023-11-01 15:37:14 +0200

    changed colorscheme, corrected capitalize 1st letter bug, added footer link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
 
   const getCurrentYear = () => {
-    return new Date().getFullYear()
-  }
+    return new Date().getFullYear();
+  };
 
   return (
     <div className='footer'>
-      <p>Text Switcher V.1 by Avspyros - {getCurrentYear()}</p>
+      <p>Text Switcher v1.1 by <a href="https://avspyros.netlify.app/" target="_blank">Avspyros</a> - {getCurrentYear()}</p>
     </div>
-  )
+  );
 }
--- src/functionality.js
+++ src/functionality.js
@@ -1,28 +1,31 @@
 export function handleUrl(input) {
-  return input.toLowerCase().replace(/[^a-z0-9α-ωΑ-ΩίϊΐόάέύϋΰήώΊΪΌΆΈΎΫΉΏ]+/g, "-").replace(/^-+|-+$/g, "")
+  return input
+    .toLowerCase()
+    .replace(/[^a-z0-9α-ωΑ-ΩίϊΐόάέύϋΰήώΊΪΌΆΈΎΫΉΏ]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 }
 
 export function handleUpperCase(input) {
-  return input.toUpperCase()
+  return input.toUpperCase();
 }
 
 export function handleLowerCase(input) {
-  return input.toLowerCase()
+  return input.toLowerCase();
 }
 
 export function handleCapitalize(input) {
   if (input) {
-    const words = input.split(" ")
+    const words = input.split(' ');
 
     for (let i = 0; i < words.length; i++) {
       if (words[i][0] !== undefined) {
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
       }
     }
-    return words.join(" ")
+    return words.join(' ');
   }
 }
 
 export function handleDash(input) {
-  return input.replace(/ +/g, "_")
+  return input.replace(/ +/g, '_');
 }
